0f1bc1de9e638857f47512fb940b3b446a7e7c1c
Joylynne Esportuno <joylynnegrace@gmail.com>
2024-09-09 22:34:55 +0800

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css"
+function Header(){
+    return(
+        <div className="header">
+        <img src="./nevermore.png" className="logo"></img>
+        <h1 className="title">Nevermore Academy: The Nightshades</h1>
+        </div>
+    );
+}
+
+export default Header
